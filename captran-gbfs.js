8f2cf8f76b4284f52a6b59910e9169c22d5f972b
James Moon <mooniker@gmail.com>
2016-07-12 01:08:05 -0500

    Add geolocation lookup for bike stations with aggregated station and status data

--- captran-gbfs.js
+++ captran-gbfs.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const request = require('request')
+const requestPromise = require('request-promise')
+const _ = require('lodash')
 
 // gbfs.json	Optional	Auto-discovery file that links to all of the other files published by the system. This file is optional, but highly recommended.
 // system_information.json	Yes	Describes the system including System operator, System location, year implemented, URLs, contact info, time zone
@@ -14,40 +16,81 @@ const request = require('request')
 // system_alerts.json	Optional	Describes current system alerts
 
 const request200 = (url, callback) => {
-  request(url, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      callback(error || new Error(response.statusCode))
-    } else {
-      callback(null, JSON.parse(body))
-    }
-  })
+  return callback // if callback supplied, return a request invokes that callback
+    ? request(url, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        callback(error || new Error(response.statusCode))
+      } else {
+        callback(null, JSON.parse(body))
+      }
+    }) // if callback not supplied, return a promise
+    : requestPromise(url, (error, response, body) => {
+      if (error || response.statusCode !== 200) throw error || new Error(response.statusCode)
+    }).then(JSON.parse)
+}
+
+const calcDistanceBetweenLatLongs = (lat1, lon1, lat2, lon2) => {
+  const p = 0.017453292519943295 // Math.PI / 180
+  const cos = Math.cos
+  let a = 0.5 - cos((lat2 - lat1) * p) / 2 +
+          cos(lat1 * p) * cos(lat2 * p) *
+          (1 - cos((lon2 - lon1) * p)) / 2
+  return 12742 * Math.asin(Math.sqrt(a)) // 2 * R; R = 6371 km
 }
 
 module.exports = class Gbfs {
   constructor (config) {
-    // this.gbfs = undefined
+    this.gbfsData = undefined
+    this.files = undefined
     if (config.gbfsUrl) {
       this.gbfsUrl = config.gbfsUrl
     } else throw new Error('GBFS.json URL not specified.')
+    console.log('created', config)
   }
 
   gbfs (callback) {
-    request200(this.gbfsUrl, callback)
+    // console.log('gbfs.json requested.')
+    if (!this.gbfsData || this.gbfsData.last_updated + this.gbfsData.ttl < Date.now()) {
+      // if gbfs data hasnt been cached or is too old, get new data
+      return request200(this.gbfsUrl, callback)
+    } else {
+      // console.log('Using cached gbfs.')
+      return callback
+        ? callback(null, this.gbfsData)
+        : Promise.resolve(this.gbfsData)
+    }
   }
 
-  query (lang, fileName, callback) {
-    this.gbfs((error, gbfs) => {
-      if (error || !gbfs) callback(error || new Error('no gbfs?'))
-      else {
-        let feeds = gbfs.data[lang].feeds
-        try {
-          var url = feeds.filter(feed => feed.name === fileName)[0].url
-        } catch (e) {
-          callback(e)
-        }
-        request200(url, callback)
+  getFile (lang, fileName, callback) {
+    let getUrlForFile = gbfs => {
+      let feeds = gbfs.data[lang].feeds
+      let url
+      url = feeds.filter(feed => feed.name === fileName)[0].url
+      if (url) {
+        return url
+      } else {
+        console.error(lang, fileName, 'not found in', feeds)
       }
-    })
+      return null
+    }
+    if (callback) {
+      this.gbfs().then(gbfs => {
+        return request200(getUrlForFile(gbfs), callback)
+      }).catch(error => callback(error))
+    } else {
+      return this.gbfs().then(gbfsData => request200(getUrlForFile(gbfsData)))
+    }
+  }
+
+  // Shortcuts for GBFS required files
+  getSystemInfo (lang, callback) {
+    return this.getFile(lang, 'system_information', callback)
+  }
+  getStationInfo (lang, callback) {
+    return this.getFile(lang, 'station_information', callback)
+  }
+  getStationStatus (lang, callback) {
+    return this.getFile(lang, 'station_status', callback)
   }
 
   dir () {
@@ -62,6 +105,33 @@ module.exports = class Gbfs {
           })
         }
       }
+    })
+  }
+
+  stations (lang, xcallback) {
+    return Promise.all([
+      this.getFile(lang, 'station_information'),
+      this.getFile(lang, 'station_status')
+    ]).then(results => {
+      let stationInfos = results[0].data.stations
+      let statuses = results[1].data.stations
+      let lastUpdated = results[0].last_updated < results[1].last_updated ? results[0].last_updated : results[1].last_updated
+      let ttl = results[0].ttl < results[1].ttl ? results[0].ttl : results[1].ttl
+      let mergedStationsData = _.map(stationInfos, item => _.extend(item, _.find(statuses, { station_id: item.station_id })))
+      return {
+        last_updated: lastUpdated,
+        ttl: ttl,
+        data: mergedStationsData
+      }
+    })
+  }
+
+  getStationsNear (lang, lat, lon, radius, xcallback) {
+    return this.stations(lang).then(stationsData => {
+      let nearbyStations = stationsData.data.filter(station => {
+        return calcDistanceBetweenLatLongs(lat, lon, station.lat, station.lon) <= radius
+      })
+      return nearbyStations
     })
   }
 
