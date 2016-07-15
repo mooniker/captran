2548556a5a11a37aa2fc438ef7bf93ec9c31f4b7
James Moon <mooniker@gmail.com>
2016-07-15 00:47:35 -0500

    Break out wrapper helper function tests into separate file

--- tests/server.tests.js
+++ tests/server.tests.js
@@ -5,14 +5,14 @@ const supertest = require('supertest')
 var server = require('../server')
 
 describe('basic http server', () => {
-  it('should respond to GET /ping with JSON-packaged "pong"', (done) => supertest(server)
+  it('should respond to GET /ping with JSON-packaged "pong"', done => supertest(server)
     .get('/ping')
     .set('Accept', 'application/json')
     .expect('Content-Type', /json/)
     .expect(200)
     .expect(JSON.stringify({ 'body': 'pong' }), done))
 
-  it('should respond to GET /foo with 404 error', (done) => supertest(server)
+  it('should respond to GET /foo with 404 error', done => supertest(server)
     .get('/foo')
     .expect(404, done))
 })
--- tests/tests.js
+++ tests/tests.js
@@ -2,64 +2,64 @@
 
 const assert = require('chai').assert
 const Wmata = require('../captran-wmata')
-var wmata = new Wmata()
+let wmata = new Wmata()
 
-describe('WMATA wrapper class\'s renderQueryUrl method', () => {
-  let query = 'stops'
-  let params = {
-    api: query,
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3'
-  }
-  let testKey = 'abcde'
-
-  it('should return a nice query URL', done => {
-    let expectedBaseUrl = 'https://api.wmata.com/Bus.svc/json/' + 'jStops'
-    assert.equal(wmata.renderQueryUrl(params, testKey),
-      expectedBaseUrl + `?key1=value1&key2=value2&key3=value3&api_key=${testKey}`)
-    assert.equal(wmata.renderQueryUrl({
-      api: 'busPositions',
-      Lat: 38.8690011,
-      Lon: -77.0544217,
-      Radius: 500
-    }, testKey),
-      `https://api.wmata.com/Bus.svc/json/jBusPositions?Lat=38.8690011&Lon=-77.0544217&Radius=500&api_key=${testKey}`)
-    done()
-  })
-
-  it('should render query URLs that have no parameters (except API key)', done => {
-    assert.equal(wmata.renderQueryUrl({
-      api: 'routes'
-    }, testKey), `https://api.wmata.com/Bus.svc/json/jRoutes?api_key=${testKey}`)
-    done()
-  })
-
-  it('should throw an error if passed an invalid query param', done => {
-    assert.throws(() => {
-      wmata.renderQueryUrl({
-        api: 'ruotes'
-      }, testKey)
-    }, /Ohnoes/)
-    done()
-  })
-})
-
-describe('WMATA API wrapper class\'s addTimestamp method', () => {
-  it('should add a timestamp to an object', done => {
-    let timeless = wmata.addTimestamp({})
-    let now = Date.now()
-    assert.isAtLeast(timeless.timestamp, now - 20)
-    assert.isAtMost(timeless.timestamp, now + 200)
-    done()
-  })
-  it(`should add a time-to-live (TTL, i.e. expiration age) to JSON data that has real-time data content (such as ${wmata.realTimeFeeds[0]})`, done => {
-    let ttlTest = wmata.addTimestamp({ [wmata.realTimeFeeds[0]]: 'stuff' })
-    assert.isOk(ttlTest.ttl)
-    assert.equal(ttlTest.ttl, wmata.ttl)
-    done()
-  })
-})
+// describe('WMATA wrapper class\'s renderQueryUrl method', () => {
+//   let query = 'stops'
+//   let params = {
+//     api: query,
+//     key1: 'value1',
+//     key2: 'value2',
+//     key3: 'value3'
+//   }
+//   let testKey = 'abcde'
+//
+//   it('should return a nice query URL', done => {
+//     let expectedBaseUrl = 'https://api.wmata.com/Bus.svc/json/' + 'jStops'
+//     assert.equal(wmata.renderQueryUrl(params, testKey),
+//       expectedBaseUrl + `?key1=value1&key2=value2&key3=value3&api_key=${testKey}`)
+//     assert.equal(wmata.renderQueryUrl({
+//       api: 'busPositions',
+//       Lat: 38.8690011,
+//       Lon: -77.0544217,
+//       Radius: 500
+//     }, testKey),
+//       `https://api.wmata.com/Bus.svc/json/jBusPositions?Lat=38.8690011&Lon=-77.0544217&Radius=500&api_key=${testKey}`)
+//     done()
+//   })
+//
+//   it('should render query URLs that have no parameters (except API key)', done => {
+//     assert.equal(wmata.renderQueryUrl({
+//       api: 'routes'
+//     }, testKey), `https://api.wmata.com/Bus.svc/json/jRoutes?api_key=${testKey}`)
+//     done()
+//   })
+//
+//   it('should throw an error if passed an invalid query param', done => {
+//     assert.throws(() => {
+//       wmata.renderQueryUrl({
+//         api: 'ruotes'
+//       }, testKey)
+//     }, /Ohnoes/)
+//     done()
+//   })
+// })
+//
+// describe('WMATA API wrapper class\'s addTimestamp method', () => {
+//   it('should add a timestamp to an object', done => {
+//     let timeless = wmata.addTimestamp({})
+//     let now = Date.now()
+//     assert.isAtLeast(timeless.timestamp, now - 20)
+//     assert.isAtMost(timeless.timestamp, now + 200)
+//     done()
+//   })
+//   it(`should add a time-to-live (TTL, i.e. expiration age) to JSON data that has real-time data content (such as ${wmata.realTimeFeeds[0]})`, done => {
+//     let ttlTest = wmata.addTimestamp({ [wmata.realTimeFeeds[0]]: 'stuff' })
+//     assert.isOk(ttlTest.ttl)
+//     assert.equal(ttlTest.ttl, wmata.ttl)
+//     done()
+//   })
+// })
 
 // requires network
 describe('WMATA API wrapper', () => {
